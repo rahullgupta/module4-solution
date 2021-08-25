4720523d8a2bbda1167af76cd2659ccf61406672
Rahul Kumar Gupta <73542002+RahulKumarGupta01@users.noreply.github.com>
2021-08-26 02:35:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ WARNING!!! WARNING!!!
 (function () {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var i = 0; i < names.length; i++) {
-    var firstLetter = names[i].charAt(0).toLowerCase()
+    var firstLetter = names[i].charAt(0).toLowerCase();
     if (firstLetter == 'j') {
       byeSpeaker.speak(names[i]);
     }
